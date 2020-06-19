76cb17090a495080078b1165b07e2aa08bf4bf11
Nguyễn Đức Hưng <hungnguyenw420@gmail.com>
2020-06-19 10:51:52 +0700

    sua quy trinh tim kiem xe + xoa file thua

--- src/DAO/CarDao.java
+++ src/DAO/CarDao.java
@@ -8,6 +8,7 @@ package DAO;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.Date;
 import Model.Car;
 
 /**
@@ -16,17 +17,21 @@ import Model.Car;
  */
 public class CarDao extends DAO {
 
-    public ArrayList<Car> searchCar(String key, int type, int brand) {
+    public ArrayList<Car> searchCar(Date receivedDate, Date returnDate, String key, int type, int brand) {
         ArrayList<Car> res = new ArrayList<Car>();
         CarTypeDao typeDao = new CarTypeDao();
         CarClassificationDao classDao = new CarClassificationDao();
-        String sql = "Select * from (Select * from tblcar where tblCarType_id = ? and tblCarClassification_id =?) t where t.name like ?";
+        String sql = "Select * from tblcar where tblCarType_id = ? and tblCarClassification_id = ?  AND name LIKE ? "
+                + "AND id NOT IN(SELECT id FROM tblbookedcar WHERE receivedDate > ? AND returnDate < ?) ";
         try {
             PreparedStatement ps = conn.prepareStatement(sql);
+            java.sql.Date sqlcheckin = new java.sql.Date(receivedDate.getTime());
+            java.sql.Date sqlcheckout = new java.sql.Date(returnDate.getTime());
             ps.setInt(1, type);
             ps.setInt(2, brand);
             ps.setString(3, "%" + key + "%");
-
+            ps.setDate(4, sqlcheckin);
+            ps.setDate(5, sqlcheckout);
             ResultSet rs = ps.executeQuery();
 
             while (rs.next()) {
